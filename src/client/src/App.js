84240338eae2d1a4eee4670fa4ccd9aee88d8eb8
RyanLin11 <ryanlin118@gmail.com>
2021-11-26 21:13:15 -0500

    Added basic auth

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import EventList from './components/EventList';
 import EventForm from './components/EventForm';
 import {Route, Routes} from "react-router-dom";
+import LoginForm from './components/Login';
+import RegisterForm from './components/Register';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<EventList />} />
-        <Route path="/add" element={<EventForm />} />
+        <Route exact path="/" element={<EventList />} />
+        <Route exact path="/add" element={<EventForm />} />
+        <Route exact path="/login" element={<LoginForm />} />
+        <Route exact path="/register" element={<RegisterForm />} />
       </Routes>
     </div>
   );
