240298f4e04325d528f57ec085f18a74ad74ee34
JacobIm28 <84198748+JacobIm28@users.noreply.github.com>
2021-12-03 16:10:35 -0500

    Event Page

--- src/client/src/Pages/Events/AddEvent.js
+++ src/client/src/Pages/Events/AddEvent.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+//import AdapterDateFns from '@mui/lab/AdapterDateFns';
+//import LocalizationProvider from '@mui/lab/LocalizationProvider';
+//import { TimePicker } from "@mui/lab";
 
 function AddEvent() {
+  const eventList = [];
+
+  const [eventName,setEventName] = useState("");
+  const [time, setTime] = useState();
+  const [details, setDetails] = useState("");
+
+  const handleSubmit = (target) => {
+    eventList.push({
+      eventName: target.eventName,
+      time: target.time,
+      details: target.details,
+    });
+  };
+
   return (
     <div>
-      <form action=""></form>
+      <ul>
+        {eventList.map}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <div className="inner-form">
+          <div>Enter event details</div>
+          <div className="form-group">
+            <label htmlFor="event-name">Event: </label>
+            <input
+              type="text"
+              value={eventName}
+              placeholder="Give your an event a name!"
+              onChange={({ target }) => setEventName(target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="time">Time: </label>
+            <input
+              type="text"
+              value={time}
+              placeholder="What time is your event?"
+              onChange={({ target }) => setTime(target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="Details">Details: </label>
+            <input
+              type="text"
+              value={details}
+              placeholder="Details"
+              onChange={({ target }) => setDetails(target.value)}
+            />
+          </div>
+          <input type="submit" value="add-event" />
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default AddEvent
+export default AddEvent;
--- src/client/src/Pages/Events/EventList.js
+++ src/client/src/Pages/Events/EventList.js
@@ -2,25 +2,35 @@ import React from "react";
 import { useState } from "react";
 import "./EventList.css";
 import Event from "../../components/Event";
+import AddEvent from "./AddEvent";
 import Home from "../Home";
 import { render } from "react-dom";
+import { Link } from "react-router-dom";
 
-function EventList(){
-
-  const [eventList, addEvent] = useState([])
+function EventList() {
+  const [eventList, addToEventList] = useState([]);
+  const [eventDetails, toggleEventDetails] = useState(false);
 
   const addEvent = () => {
-    render(<AddEvent />);
-  }
+    toggleEventDetails(!eventDetails);
+  };
 
+  // when they submit event details, send to database, and then when I render it looks for the name in database
 
-
-  return(
+  return (
     <div className="event-list">
       <h1>Upcoming Events</h1>
       <div className="buttons">
-        <button className="addd-event" onClick={addEvent}>Add Event</button>
+        <button className="add-event" onClick={addEvent}>
+          Add Event
+        </button>
+        {eventDetails ? <AddEvent /> : <></>}
       </div>
+      <ul>
+        {eventList.map((event) => (
+          <li>{event}</li>
+        ))}
+      </ul>
     </div>
   );
 }
