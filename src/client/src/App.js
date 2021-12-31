af6d6f52c3b31f4ef100dd6af1ac1e37eacdd8f8
Joseph Wang <44278515+EmeraldEntities@users.noreply.github.com>
2021-12-31 00:38:28 -0500

    Revert "Merge branch 'backend-stuff'"
    This reverts commit da5a5de5f243e34c80a8c2992a5ed2bba62edc38, reversing
    changes made to b78da52dac0344f8af5667afb7a80d2aea691d0a.

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,44 +1,11 @@
-import { useState, useEffect } from "react";
-import React from "react";
-import "./App.css";
-import Home from "./Pages/Home";
-import LoginForm from "./Pages/LoginForm";
-import NavBar from "./components/NavBar";
-import axios from "axios";
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/Login';
 
-// require('react-dom');
-// window.React2 = require('react');
-// console.log(window.React1 === window.React2);
-
-
-export default function App() {
-
-  const [user, setUser] = useState({username: "", password: "", email: ""});
-
-  useEffect(() => {
-    const loggedInUser = localStorage.getItem("user");
-    if (loggedInUser) {
-      const foundUser = JSON.parse(loggedInUser);
-      setUser(foundUser);
-      console.log(foundUser);
-    }
-  }, []);
-  
-  const Logout = () => {
-    setUser({ username: "", email: "" });
-  };
-
-  return (
-    <div className="App">
-      {user.username != "" ? (
-        <div className="welcome">
-          <h1>
-            Welcome, <span>{user.username}</span>
-          </h1>
-        </div>
-      ) : (
-        <LoginForm />
-      )}
-    </div>
-  );
+const App = () => {
+  <div>
+    <Login />
+  </div>
 }
+
+export default App;
--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,33 +1,15 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import LoginForm from "./Pages/LoginForm";
-import Home from "./Pages/Home";
-import Profile from "./Pages/Profile";
-import EventList from "./Pages/Events/EventList";
-import NavBar from "./components/NavBar";
-import ErrorBoundary from "./components/ErrorBoundary";
 import reportWebVitals from "./reportWebVitals";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.render (
-  <div >
-    <BrowserRouter>
-    <NavBar />
-    <ErrorBoundary >
-      <Routes>
-        <Route exact path="/" element={<App />} />
-        <Route exact path="home" element={<Home />} />
-        <Route exact path="loginform" element={<LoginForm />} />
-        <Route exact path="profile" element={<Profile />} />
-        <Route exact path="eventlist" element={<EventList />} />
-      </Routes>
-      </ErrorBoundary>
-    </BrowserRouter>
-  </div>,
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
   document.getElementById("root")
-);
+);//adkas
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
