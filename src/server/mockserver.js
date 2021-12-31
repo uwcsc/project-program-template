2a318bf0ca654e74c93cb1692cc0135e5576c94a
Joseph Wang <44278515+EmeraldEntities@users.noreply.github.com>
2021-12-31 00:38:20 -0500

    Revert "clean-up non-used server"
    This reverts commit 1ad3b482b1a51e9e03f369e4a828d3cef80b0bbb.

---
+++ src/server/mockserver.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const { application } = require("express");
+require("dotenv").config();
+
+const dao = require("./db/dao");
+const { dummyEvent } = require("./db/schema");
+const { ObjectId } = require("bson");
+
+const port = process.env.SERVER_PORT;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, async () => {
+	console.log(`Server is now running on port ${port}`);
+
+	await dao.connectToServer();
+	// let aaa = new ObjectId();
+	// let bbb = aaa.valueOf();
+	// let ccc = new ObjectId(bbb);
+	// console.log(aaa)
+	// console.log(ccc)
+	// console.log(aaa.equals(ccc));
+
+	// let cool = await dao.getEvent(new ObjectId("61a968cdf4168796656e721e"));
+	// console.log(cool)
+
+	// console.log(await dao.addEvent(dummyEvent));
+});
